736ac3ed15e55b8d9689086e338052f5ab4e47b6
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-04-03 13:27:15 +0100

    [New] Add insertion sort

---
+++ insertion_sort.h
@@ -0,0 +1,12 @@
+int* insertion_sort(int arr[], int s) {
+  for (int i = 0; i < s; i++) {
+    int j = i;
+    while (j > 0 && arr[j - 1] > arr[j]) {
+      int temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+      j--;
+    }
+  }
+  return arr;
+}
