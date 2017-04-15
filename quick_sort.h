ed2511a6fd041f95bcb707eb50b819642c50b17a
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-04-15 11:31:07 +0100

    [New] Add quick sort

---
+++ quick_sort.h
@@ -0,0 +1,37 @@
+void quick_sort(int array[], int first_index, int last_index) {
+  // declaring index variables
+  int pivotIndex, temp, index_a, index_b;
+
+  if (first_index < last_index) {
+    // assigning first element index as pivot element
+    pivotIndex = first_index;
+    index_a = first_index;
+    index_b = last_index;
+
+    // Sorting in Ascending order with quick sort
+    while (index_a < index_b) {
+      while (array[index_a] <= array[pivotIndex] && index_a < last_index) {
+        index_a++;
+      }
+      while (array[index_b] > array[pivotIndex]) {
+        index_b--;
+      }
+
+      if (index_a < index_b) {
+        // Swapping operation
+        temp = array[index_a];
+        array[index_a] = array[index_b];
+        array[index_b] = temp;
+      }
+    }
+
+    // At the end of first iteration, swap pivot element with index_b element
+    temp = array[pivotIndex];
+    array[pivotIndex] = array[index_b];
+    array[index_b] = temp;
+
+    // Recursive call for quick sort, with partitioning
+    quick_sort(array, first_index, index_b - 1);
+    quick_sort(array, index_b + 1, last_index);
+  }
+}
