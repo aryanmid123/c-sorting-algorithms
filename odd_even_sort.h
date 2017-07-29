5b762c8d673c363e39297070923c1b95f7e83142
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-29 10:00:05 +0100

    [Fix] Add correct odd_even_sort

--- odd_even_sort.h
+++ odd_even_sort.h
@@ -11,16 +11,14 @@ Worst case space complexity: O(1)
 #include <stdbool.h>
 
 void odd_even_sort(int arr[], int n) {
+  bool sorted = false;  // Initially array is unsorted
   int temp;
-  int i;
-  int j;
-  int gap = n;
-  int shrink = 3;
-  bool sorted = false;
 
   while (!sorted) {
     sorted = true;
-    for (i = 1; i < n; i += 2) {
+
+    // Perform Bubble sort on odd indexed element
+    for (int i = 1; i <= n - 2; i += 2) {
       if (arr[i] > arr[i + 1]) {
         temp = arr[i + 1];
         arr[i + 1] = arr[i];
@@ -29,7 +27,8 @@ void odd_even_sort(int arr[], int n) {
       }
     }
 
-    for (i = 0; i < n; i += 2) {
+    // Perform Bubble sort on even indexed element
+    for (int i = 0; i <= n - 2; i += 2) {
       if (arr[i] > arr[i + 1]) {
         temp = arr[i + 1];
         arr[i + 1] = arr[i];
